e2546a8b73c42c26478906f1b59e451bba1aca4f
mldamico <matias.lioneldamico@gmail.com>
2023-08-11 20:19:09 -0300

    Zustand installation and setup with useParamsStore

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -5,33 +5,46 @@ import { Auction, PagedResult } from "@/types";
 import AppPagination from "../components/AppPagination";
 import { getData } from "../actions/auctionAction";
 import Filters from "./Filters";
-
+import { useParamsStore } from "@/hooks/useParamsStore";
+import { shallow } from "zustand/shallow";
+import qs from "query-string";
 export default function Listings() {
-  const [auctions, setAuctions] = useState<Auction[]>([]);
-  const [pageCount, setPageCount] = useState(0);
-  const [pageNumber, setPageNumber] = useState(1);
-  const [pageSize, setPageSize] = useState(4);
-  useEffect(() => {
-    getData(pageNumber, pageSize).then((data) => {
-      setAuctions(data.results);
-      setPageCount(data.pageCount);
-    });
-  }, [pageNumber, pageSize]);
+  const [data, setData] = useState<PagedResult<Auction>>();
+  const params = useParamsStore(
+    (state) => ({
+      pageNumber: state.pageNumber,
+      pageSize: state.pageSize,
+      searchTerm: state.searchTerm,
+    }),
+    shallow
+  );
+  const setParams = useParamsStore((state) => state.setParams);
+  const url = qs.stringifyUrl({ url: "", query: params });
 
-  if (auctions.length === 0) return <h3>Loading...</h3>;
+  function setPageNumber(pageNumber: number) {
+    setParams({ pageNumber });
+  }
+
+  useEffect(() => {
+    getData(url).then((data) => {
+      setData(data);
+    });
+  }, [url]);
+
+  if (!data) return <h3>Loading...</h3>;
 
   return (
     <>
-      <Filters pageSize={pageSize} setPageSize={setPageSize} />
+      <Filters />
       <div className="grid grid-cols-4 gap-6">
-        {auctions.map((auction: Auction) => (
+        {data.results.map((auction: Auction) => (
           <AuctionCard auction={auction} key={auction.id} />
         ))}
       </div>
       <div className="flex justify-center mt-4">
         <AppPagination
-          pageCount={pageCount}
-          currentPage={pageNumber}
+          pageCount={data.pageCount}
+          currentPage={params.pageNumber}
           pageChanged={setPageNumber}
         />
       </div>
