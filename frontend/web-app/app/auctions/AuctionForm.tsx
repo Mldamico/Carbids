76ca6e472c28d15ae31a327f21725357ce5afed2
mldamico <matias.lioneldamico@gmail.com>
2023-08-13 18:09:25 -0300

    Fetch wrapper

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -3,6 +3,7 @@ import { Button, TextInput } from "flowbite-react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Input from "../components/Input";
 import { useEffect } from "react";
+import DateInput from "../components/DateInput";
 
 export default function AuctionForm() {
   const {
@@ -71,11 +72,12 @@ export default function AuctionForm() {
           type="number"
           rules={{ required: "Reserve price is required" }}
         />
-        <Input
+        <DateInput
           label="Auction end date/time"
           name="auctionEnd"
           control={control}
-          type="date"
+          dateFormat={"dd MMMM yyyy h:mm a"}
+          showTimeSelect
           rules={{ required: "Auction end date is required" }}
         />
       </div>
