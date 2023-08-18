8502b57c64eebd962fa9f99ef2f5fc51f52bb0cf
mldamico <matias.lioneldamico@gmail.com>
2023-08-18 13:57:54 -0300

    Bid item component

--- frontend/web-app/app/auctions/details/[id]/page.tsx
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -9,6 +9,7 @@ import DetailedSpecs from "./DetailedSpecs";
 import { getCurrentUser } from "@/app/actions/authActions.";
 import EditButton from "./EditButton";
 import DeleteButton from "./DeleteButton";
+import BidItem from "./BidItem";
 
 export default async function Details({ params }: { params: { id: string } }) {
   const user = await getCurrentUser();
@@ -39,9 +40,7 @@ export default async function Details({ params }: { params: { id: string } }) {
         <div className="border-2 rounded-lg p-2 bg-gray-100">
           <Heading title="Bids" />
           {bids.map((bid) => (
-            <p key={bid.id}>
-              {bid.bidder} - {bid.amount}
-            </p>
+            <BidItem key={bid.id} bid={bid} />
           ))}
         </div>
       </div>
