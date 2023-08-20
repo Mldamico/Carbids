6a91f98376726718da46f8e8899e1e51c3bc7fbf
mldamico <matias.lioneldamico@gmail.com>
2023-08-20 16:14:11 -0300

    Finished client app

--- frontend/web-app/app/auctions/CountdownTimer.tsx
+++ frontend/web-app/app/auctions/CountdownTimer.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { useBidStore } from "@/hooks/useBidsStore";
+import { usePathname } from "next/navigation";
 import Countdown, { zeroPad } from "react-countdown";
 
 interface RendererProps {
@@ -45,9 +47,21 @@ interface Props {
 }
 
 export default function CountdownTimer({ auctionEnd }: Props) {
+  const setOpen = useBidStore((state) => state.setOpen);
+  const pathname = usePathname();
+
+  function auctionFinished() {
+    if (pathname.startsWith("/auctions/details")) {
+      setOpen(false);
+    }
+  }
   return (
     <div>
-      <Countdown date={auctionEnd} renderer={renderer} />
+      <Countdown
+        date={auctionEnd}
+        renderer={renderer}
+        onComplete={auctionFinished}
+      />
     </div>
   );
 }
