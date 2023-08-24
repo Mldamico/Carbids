c6142671d1295beea5f165ccf064c1ec47ecdc74
mldamico <matias.lioneldamico@gmail.com>
2023-08-24 13:15:06 -0300

    Integration test setup

--- tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
+++ tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
@@ -1,8 +1,39 @@
+using AuctionService.Data;
+using AuctionService.IntegrationTests.Util;
+using MassTransit;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
 
 namespace AuctionService.IntegrationTests.Fixtures;
 
-public class CustomWebAppFactory : WebApplicationFactory<Program>
+public class CustomWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
 {
-    
+    private PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder().Build();
+
+    public async Task InitializeAsync()
+    {
+        await _postgreSqlContainer.StartAsync();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.RemoveDbContext<AuctionDbContext>();
+            services.AddDbContext<AuctionDbContext>(options =>
+            {
+                options.UseNpgsql(_postgreSqlContainer.GetConnectionString());
+            });
+
+            services.AddMassTransitTestHarness();
+
+            services.EnsureCreated<AuctionDbContext>();
+        });
+    }
+
+    public Task DisposeAsync() => _postgreSqlContainer.DisposeAsync().AsTask();
 }
