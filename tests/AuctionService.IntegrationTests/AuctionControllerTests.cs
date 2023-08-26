14e15d1ba44a4bdc6875e585e2c782544f791f1c
mldamico <matias.lioneldamico@gmail.com>
2023-08-26 16:23:02 -0300

    Create auction tests

---
+++ tests/AuctionService.IntegrationTests/AuctionControllerTests.cs
@@ -0,0 +1,134 @@
+using System.Net;
+using System.Net.Http.Json;
+using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.IntegrationTests.Fixtures;
+using AuctionService.IntegrationTests.Util;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuctionService.IntegrationTests;
+
+public class AuctionControllerTests : IClassFixture<CustomWebAppFactory>, IAsyncLifetime
+{
+    private readonly CustomWebAppFactory _factory;
+    private readonly HttpClient _httpClient;
+    private const string GT_ID = "afbee524-5972-4075-8800-7d1f9d7b0a0c";
+
+    public AuctionControllerTests(CustomWebAppFactory factory)
+    {
+        _factory = factory;
+        _httpClient = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task GetAuctions_ShouldReturn3Auctions()
+    {
+        var response = await _httpClient.GetFromJsonAsync<List<AuctionDto>>("/api/auctions");
+        Assert.Equal(3, response.Count);
+    }
+
+    [Fact]
+    public async Task GetAuctionById_WithValidId_ShouldReturnAuction()
+    {
+        var response = await _httpClient.GetFromJsonAsync<AuctionDto>($"/api/auctions/{GT_ID}");
+        Assert.Equal("GT", response.Model);
+    }
+
+    [Fact]
+    public async Task GetAuctionById_WithInvalidId_ShouldReturn404()
+    {
+        var response = await _httpClient.GetAsync($"/api/auctions/{Guid.NewGuid()}");
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetAuctionById_WithInvalidGuid_ShouldReturn400()
+    {
+        var response = await _httpClient.GetAsync($"/api/auctions/notvalid");
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task CreateAuction_WithNoAuth_ShouldReturn401()
+    {
+        var auction = new CreateAuctionDto {Make = "test"};
+
+        var response = await _httpClient.PostAsJsonAsync($"/api/auctions", auction);
+
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task CreateAuction_WithAuth_ShouldReturn201()
+    {
+        var auction = GetAuctionForCreate();
+        _httpClient.SetFakeJwtBearerToken(AuthHelper.GetBearerForUser("bob"));
+
+        var response = await _httpClient.PostAsJsonAsync($"/api/auctions", auction);
+
+        response.EnsureSuccessStatusCode();
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        var createdAuction = await response.Content.ReadFromJsonAsync<AuctionDto>();
+        Assert.Equal("bob", createdAuction.Seller);
+    }
+
+    [Fact]
+    public async Task CreateAuction_WithInvalidCreateAuctionDto_ShouldReturn400()
+    {
+        var auction = new CreateAuctionDto {Make = "test"};
+        _httpClient.SetFakeJwtBearerToken(AuthHelper.GetBearerForUser("bob"));
+
+        var response = await _httpClient.PostAsJsonAsync($"/api/auctions", auction);
+
+    
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+  
+    }
+
+    [Fact]
+    public async Task UpdateAuction_WithValidUpdateDtoAndUser_ShouldReturn200()
+    {
+        var auction = GetAuctionForCreate();
+        _httpClient.SetFakeJwtBearerToken(AuthHelper.GetBearerForUser("bob"));
+
+        var response = await _httpClient.PutAsJsonAsync($"/api/auctions/{GT_ID}", auction);
+
+        response.EnsureSuccessStatusCode();
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task UpdateAuction_WithValidUpdateDtoAndInvalidUser_ShouldReturn403()
+    {
+        var auction = GetAuctionForCreate();
+        _httpClient.SetFakeJwtBearerToken(AuthHelper.GetBearerForUser("rob"));
+
+        var response = await _httpClient.PutAsJsonAsync($"/api/auctions/{GT_ID}", auction);
+
+        Assert.Equal(HttpStatusCode.Forbidden, response.StatusCode);
+    }
+
+    public Task InitializeAsync() => Task.CompletedTask;
+
+    public Task DisposeAsync()
+    {
+        using var scope = _factory.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<AuctionDbContext>();
+        DbHelper.ReInitDbForTests(db);
+        return Task.CompletedTask;
+    }
+
+    private CreateAuctionDto GetAuctionForCreate()
+    {
+        return new CreateAuctionDto
+        {
+            Make = "test",
+            Model = "testModel",
+            ImageUrl = "test",
+            Colour = "test",
+            Kilometer = 10,
+            Year = 10,
+            ReservePrice = 10,
+        };
+    }
+}
--- tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
+++ tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
@@ -7,12 +7,13 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Testcontainers.PostgreSql;
+using WebMotions.Fake.Authentication.JwtBearer;
 
 namespace AuctionService.IntegrationTests.Fixtures;
 
 public class CustomWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
 {
-    private PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder().Build();
+    private readonly PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder().Build();
 
     public async Task InitializeAsync()
     {
@@ -24,6 +25,7 @@ public class CustomWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetim
         builder.ConfigureTestServices(services =>
         {
             services.RemoveDbContext<AuctionDbContext>();
+
             services.AddDbContext<AuctionDbContext>(options =>
             {
                 options.UseNpgsql(_postgreSqlContainer.GetConnectionString());
@@ -32,8 +34,11 @@ public class CustomWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetim
             services.AddMassTransitTestHarness();
 
             services.EnsureCreated<AuctionDbContext>();
+
+            services.AddAuthentication(FakeJwtBearerDefaults.AuthenticationScheme)
+                .AddFakeJwtBearer(opt => { opt.BearerValueType = FakeJwtBearerBearerValueType.Jwt; });
         });
     }
 
-    public Task DisposeAsync() => _postgreSqlContainer.DisposeAsync().AsTask();
+    Task IAsyncLifetime.DisposeAsync() => _postgreSqlContainer.DisposeAsync().AsTask();
 }
